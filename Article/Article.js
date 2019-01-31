d09251301156f577692821e5b2c1998b22baa7d3
lydiathornton <lydia.d.thornton@gmail.com>
2019-01-30 22:32:25 -0800

    toggle works both ways

--- Article/Article.js
+++ Article/Article.js
@@ -9,19 +9,26 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
-
-
-
     this.expandButton.addEventListener('click', () => this.expandArticle());
+
+
+
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
-    this.expandButton.textContent = 'close';
+    if (this.domElement.classList.contains('article-open')) {
+      this.domElement.classList.toggle('article-open')
+      this.expandButton.textContent = 'expand'
+    } 
+    else {
+      this.domElement.classList.toggle('article-open')
+      this.expandButton.textContent = 'close'
+    }
+  } 
+  
 }
-} 
+ 
 
 /* START HERE: 
 
